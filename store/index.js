3410aefeb57291acfcceae055769977cb0317cb5
İsmail <ismailakcelep@gmail.com>
2022-09-12 13:07:53 +0300

    Update index.js

--- store/index.js
+++ store/index.js
@@ -1,1 +1,21 @@
-// import Vuex from "vuex"
+import Vuex from "vuex";
+const createStore = () => {
+return new Vuex.Store({
+    state:{
+        fetchedPost:[]
+    },
+    mutations:{
+        setPosts(state,posts){
+            state.fetchedPost = posts
+        }
+    },
+    actions:{
+        setPosts(vuexContext,posts){
+            vuexContext.commit("setPosts",posts)
+        }
+    },
+    getters:{}
+})
+}
+
+export default createStore
